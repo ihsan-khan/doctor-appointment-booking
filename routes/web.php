9c37b4fa5b051940ed9301194a49d297686cf343
ihsan-khan <ehsankhan0577@gmail.com>
2025-12-18 17:41:05 +0500

    patient controller were creataed

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+Route::group(['middleware' => 'patient'], function(){
+    Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified','patient'])
     ->name('dashboard');
+
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
