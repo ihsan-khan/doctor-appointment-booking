c8a492bbec4b63728c33e5f4f4d8d93c19992891
ihsan-khan <ehsankhan0577@gmail.com>
2025-12-18 16:58:29 +0500

    role column were added to users table

--- database/migrations/2025_12_18_115447_add_role_column_to_users_table.php
+++ database/migrations/2025_12_18_115447_add_role_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'user', 'patient'])->default('user')->after('email');
+            $table->enum('role', ['admin', 'doctor', 'patient'])->default('patient')->after('email');
         });
     }
 
