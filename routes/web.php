d9de8c4a6b8e3008ec66db4d753ae468c6a3bb4b
ihsan-khan <ehsankhan0577@gmail.com>
2025-12-20 12:29:37 +0500

    doctor and admin controller created

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::group(['middleware' => 'patient'], function(){
+Route::group(['middleware' => 'patient'], function () {
     Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified','patient'])
-    ->name('dashboard');
-
+        ->middleware(['auth', 'verified', 'patient'])
+        ->name('dashboard');
 });
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+
+Route::group(['middleware' => 'doctor'], function () {
+
+    Route::get('/doctor/dashboard', [DoctorController::class, 'loadDoctorDashboard'])
+        ->name('doctor-dashboard');
+});
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'loadAdminDashboard'])
+        ->name('admin-dashboard');
+});
+
+require __DIR__ . '/auth.php';
