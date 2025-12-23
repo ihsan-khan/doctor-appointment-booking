b5624b35c66548cadced9e1cacbeb0c17a132d45
ihsan-khan <ehsankhan0577@gmail.com>
2025-12-23 16:37:29 +0500

    Specialist Cards component were  added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
         <div class="py-5 bg-gray-200">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <livewire:featured-doctors :speciality_id="0" />
+                    {{-- <livewire:featured-doctors :speciality_id="0" /> --}}
                 </div>
             </div>
         </div>
